2a01ab416867d8997506de2948acabdee80bf782
Zac Sweers <zac.sweers@gmail.com>
2018-05-19 19:11:44 -0700

    Add moshi reflect and codegen tests

--- src/jmh/java/com/example/SpeedTest.java
+++ src/jmh/java/com/example/SpeedTest.java
@@ -7,14 +7,27 @@ import com.esotericsoftware.kryo.io.Output;
 import com.esotericsoftware.kryo.serializers.CompatibleFieldSerializer;
 import com.example.adapter.GeneratedJsonAdapterFactory;
 import com.example.adapter.GeneratedTypeAdapterFactory;
+import com.example.kotlinx_serialization.Response;
 import com.example.model_av.ResponseAV;
-import com.example.model_reflective.Response;
+import com.example.moshiKotlinCodegen.KCGResponse;
+import com.example.moshiKotlinReflective.KRResponse;
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.squareup.moshi.Moshi;
-
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import okio.Buffer;
+import okio.BufferedSink;
+import okio.BufferedSource;
 import org.objenesis.strategy.StdInstantiatorStrategy;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
@@ -25,18 +38,7 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 
-import java.io.ByteArrayOutputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.Writer;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
-
-import okio.Buffer;
-import okio.BufferedSink;
-import okio.BufferedSource;
-
+@SuppressWarnings("ALL")
 public class SpeedTest {
 
     @State(Scope.Benchmark)
@@ -107,6 +109,45 @@ public class SpeedTest {
     }
 
     @State(Scope.Benchmark)
+    public static class ReflectiveMoshiKotlin {
+
+        @Setup
+        public void setupTrial() throws Exception {
+            moshi = new Moshi.Builder()
+                    .add(new KotlinJsonAdapterFactory())
+                    .build();
+            URL url = Resources.getResource("largesample.json");
+            json = Resources.toString(url, Charsets.UTF_8);
+            response = moshi
+                    .adapter(KRResponse.class)
+                    .fromJson(json);
+        }
+
+        public Moshi moshi;
+        public String json;
+        public KRResponse response;
+    }
+
+    @State(Scope.Benchmark)
+    public static class CodegenMoshiKotlin {
+
+        @Setup
+        public void setupTrial() throws Exception {
+            moshi = new Moshi.Builder()
+                    .build();
+            URL url = Resources.getResource("largesample.json");
+            json = Resources.toString(url, Charsets.UTF_8);
+            response = moshi
+                    .adapter(KCGResponse.class)
+                    .fromJson(json);
+        }
+
+        public Moshi moshi;
+        public String json;
+        public KCGResponse response;
+    }
+
+    @State(Scope.Benchmark)
     public static class AVMoshiBuffer {
 
         @Setup
@@ -133,6 +174,63 @@ public class SpeedTest {
         public BufferedSource bufferedSource;
         public BufferedSink bufferedSink;
         public ResponseAV response;
+    }
+
+    @State(Scope.Benchmark)
+    public static class ReflectiveMoshiKotlinBuffer {
+
+        @Setup
+        public void setupTrial() throws Exception {
+            moshi = new Moshi.Builder()
+                    .add(new KotlinJsonAdapterFactory())
+                    .build();
+            URL url = Resources.getResource("largesample.json");
+            json = Resources.toString(url, Charsets.UTF_8);
+
+            response = moshi
+                    .adapter(KRResponse.class)
+                    .fromJson(json);
+        }
+
+        @Setup(Level.Invocation)
+        public void setupIteration() {
+            bufferedSource = new Buffer().writeUtf8(json);
+            bufferedSink = new Buffer();
+        }
+
+        public Moshi moshi;
+        public String json;
+        public BufferedSource bufferedSource;
+        public BufferedSink bufferedSink;
+        public KRResponse response;
+    }
+
+    @State(Scope.Benchmark)
+    public static class CodegenMoshiKotlinBuffer {
+
+        @Setup
+        public void setupTrial() throws Exception {
+            moshi = new Moshi.Builder()
+                    .build();
+            URL url = Resources.getResource("largesample.json");
+            json = Resources.toString(url, Charsets.UTF_8);
+
+            response = moshi
+                    .adapter(KCGResponse.class)
+                    .fromJson(json);
+        }
+
+        @Setup(Level.Invocation)
+        public void setupIteration() {
+            bufferedSource = new Buffer().writeUtf8(json);
+            bufferedSink = new Buffer();
+        }
+
+        public Moshi moshi;
+        public String json;
+        public BufferedSource bufferedSource;
+        public BufferedSink bufferedSink;
+        public KCGResponse response;
     }
 
     @State(Scope.Benchmark)
@@ -212,31 +310,46 @@ public class SpeedTest {
         public ResponseAV response;
         public Class<ResponseAV> responseClazz;
     }
+
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
-    public String kserializer_toJson(KSerializer param) {
+    public String kserializer_toJson(KSerializer param) throws IOException {
         return param.response.stringify();
     }
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
-    public String moshi_reflective_toJson(ReflectiveMoshi param) {
+    public String moshi_reflective_toJson(ReflectiveMoshi param) throws IOException {
         return param.moshi.adapter(Response.class).toJson(param.response);
     }
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
-    public String moshi_streaming_toJson(AVMoshi param) {
+    public String moshi_autovalue_toJson(AVMoshi param) throws IOException {
         return param.moshi.adapter(ResponseAV.class).toJson(param.response);
     }
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
-    public BufferedSink moshi_streaming_toJson_buffer(AVMoshiBuffer param) throws Exception {
+    public String moshi_kotlin_reflective_toJson(ReflectiveMoshiKotlin param) throws IOException {
+        return param.moshi.adapter(KRResponse.class).toJson(param.response);
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public String moshi_kotlin_codegen_toJson(CodegenMoshiKotlin param) throws IOException {
+        return param.moshi.adapter(KCGResponse.class).toJson(param.response);
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public BufferedSink moshi_autovalue_buffer_toJson(AVMoshiBuffer param) throws IOException {
         param.moshi.adapter(ResponseAV.class).toJson(param.bufferedSink, param.response);
         return param.bufferedSink;
     }
@@ -244,21 +357,37 @@ public class SpeedTest {
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
-    public String gson_reflective_toJson(ReflectiveGson param) {
+    public BufferedSink moshi_kotlin_reflective_buffer_toJson(ReflectiveMoshiKotlinBuffer param) throws IOException {
+        param.moshi.adapter(KRResponse.class).toJson(param.bufferedSink, param.response);
+        return param.bufferedSink;
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public BufferedSink moshi_kotlin_codegen_buffer_toJson(CodegenMoshiKotlinBuffer param) throws IOException {
+        param.moshi.adapter(KCGResponse.class).toJson(param.bufferedSink, param.response);
+        return param.bufferedSink;
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public String gson_reflective_toJson(ReflectiveGson param) throws IOException {
         return param.gson.toJson(param.response);
     }
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
-    public String gson_streaming_toJson(AVGson param) {
+    public String gson_autovalue_toJson(AVGson param) throws IOException {
         return param.gson.toJson(param.response);
     }
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
-    public Writer gson_streaming_toJson_buffer(AVGsonBuffer param) {
+    public Writer gson_autovalue_buffer_toJson(AVGsonBuffer param) throws IOException {
         param.gson.toJson(param.response, param.sink);
         return param.sink;
     }
@@ -266,7 +395,7 @@ public class SpeedTest {
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
-    public byte[] kryo_toBytes(KryoScope param) {
+    public byte[] kryo_toBytes(KryoScope param) throws IOException {
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         Output output = new Output(stream);
         param.kryo.writeObject(output, param.response);
@@ -277,56 +406,84 @@ public class SpeedTest {
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
-    public Response kserializer_fromJson(KSerializer param) {
+    public Response kserializer_string_fromJson(KSerializer param) throws IOException {
         return Response.parse(param.json);
     }
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
-    public Response moshi_reflective_fromJson(ReflectiveMoshi param) throws Exception {
+    public Response moshi_reflective_string_fromJson(ReflectiveMoshi param) throws Exception {
         return param.moshi.adapter(Response.class).fromJson(param.json);
     }
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
-    public ResponseAV moshi_streaming_fromJson(AVMoshi param) throws Exception {
+    public ResponseAV moshi_autovalue_string_fromJson(AVMoshi param) throws Exception {
         return param.moshi.adapter(ResponseAV.class).fromJson(param.json);
     }
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
-    public ResponseAV moshi_streaming_fromJson_buffer(AVMoshiBuffer param) throws Exception {
+    public KRResponse moshi_kotlin_reflective_string_fromJson(ReflectiveMoshiKotlin param) throws Exception {
+        return param.moshi.adapter(KRResponse.class).fromJson(param.json);
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public KCGResponse moshi_kotlin_codegen_string_fromJson(CodegenMoshiKotlin param) throws Exception {
+        return param.moshi.adapter(KCGResponse.class).fromJson(param.json);
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public ResponseAV moshi_autovalue_buffer_fromJson(AVMoshiBuffer param) throws IOException {
         return param.moshi.adapter(ResponseAV.class).fromJson(param.bufferedSource);
     }
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
-    public Response gson_reflective_fromJson(ReflectiveGson param) {
+    public KRResponse moshi_kotlin_reflective_buffer_fromJson(ReflectiveMoshiKotlinBuffer param) throws IOException {
+        return param.moshi.adapter(KRResponse.class).fromJson(param.bufferedSource);
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public KCGResponse moshi_kotlin_codegen_buffer_fromJson(CodegenMoshiKotlinBuffer param) throws IOException {
+        return param.moshi.adapter(KCGResponse.class).fromJson(param.bufferedSource);
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public Response gson_reflective_string_fromJson(ReflectiveGson param) throws IOException {
         return param.gson.fromJson(param.json, Response.class);
     }
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
-    public ResponseAV gson_streaming_fromJson(AVGson param) {
+    public ResponseAV gson_autovalue_string_fromJson(AVGson param) throws IOException {
         return param.gson.fromJson(param.json, ResponseAV.class);
     }
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
-    public ResponseAV gson_streaming_fromJson_buffer(AVGsonBuffer param) {
+    public ResponseAV gson_autovalue_buffer_fromJson(AVGsonBuffer param) throws IOException {
         return param.gson.fromJson(param.source, ResponseAV.class);
     }
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
-    public ResponseAV kryo_fromBytes(KryoScope param) throws ClassNotFoundException {
+    public ResponseAV kryo_fromBytes(KryoScope param) throws IOException {
         return param.kryo.readObject(new Input(param.bytes), param.responseClazz);
     }
 
